f6878962d474758862a33e61b102a0d3b6f17b65
Chethanvr06 <chethanvr06@gmail.com>
2025-01-31 14:38:56 +0530

    Update remote_backend_s3.tf

--- remote_backend_s3.tf
+++ remote_backend_s3.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-jenkins-remote-state-bucket-123456789"
+    bucket = "dev-proj-1-jenkins-remote-state-bucket-12345678"
     key    = "devops-project-1/jenkins/terraform.tfstate"
-    region = "us-east-1"
+    region = "eu-central-1"
   }
 }
